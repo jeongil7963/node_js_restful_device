36e40b76870d022f05b080f39fda054e0bcd211e
양정일 <ya63kr@nate.com>
2017-12-04 16:18:15 +0900

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var http = require('http');
 http.post = require('http-post');
+var user_token = '839ca2c5d60342309677f6f65ffc809c'
+var api_key = '58a7ff45425f4d6d809c023ee1790aa2'
 
-http.post('http://192.168.0.6:3000/setting/search', { user_token: '839ca2c5d60342309677f6f65ffc809c', api_key: '58a7ff45425f4d6d809c023ee1790aa2' }, function(res){
+http.post('http://192.168.0.6:3000/setting/search', { "user_token" : user_token, "api_key" : api_key }, function(res){
 	res.setEncoding('utf8');
 	res.on('data', function(chunk) {
 		console.log(chunk);
