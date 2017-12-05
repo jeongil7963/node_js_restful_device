14f42e0aee57c1cfdb27eb08ce3b78d27ade9713
양정일 <ya63kr@nate.com>
2017-12-05 11:40:40 +0900

    commit

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var http = require('http');
-http.post = require('http-post');
+//user_token
 var user_token = '839ca2c5d60342309677f6f65ffc809c'
 var api_key = '58a7ff45425f4d6d809c023ee1790aa2'
+
+var http = require('http');
+http.post = require('http-post');
+var config = require('./config.json');
+var water_stop_time = config.water_stop_time; // water_stop_time 값 설정
+var shooting_time = config.shooting_time; //shooting time 값 설정
+
+var current_min; // 현재 시각 '분'
+var sub_min; // 촬영 시작 전 시간
+var camera_interval; // camera 모듈 반복 제어
+
 
 http.post('http://192.168.0.6:3000/setting/search', { "user_token" : user_token, "api_key" : api_key }, function(res){
 	res.setEncoding('utf8');
@@ -9,3 +19,6 @@ http.post('http://192.168.0.6:3000/setting/search', { "user_token" : user_token,
 		console.log(chunk);
 	});
 });
+
+console.log("water_stop_time : " + water_stop_time);
+console.log("shooting_time : " + shooting_time);
