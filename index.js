0990ad3af39d86fdf0cf9db373fff0861e9518c7
양정일 <ya63kr@nate.com>
2017-12-05 11:57:28 +0900

    commit

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ http.post = require('http-post');
 var config = require('./config.json');
 var water_stop_time = config.water_stop_time; // water_stop_time 값 설정
 var shooting_time = config.shooting_time; //shooting time 값 설정
-var json = require('json');
 var current_min; // 현재 시각 '분'
 var sub_min; // 촬영 시작 전 시간
 var camera_interval; // camera 모듈 반복 제어
@@ -17,7 +16,8 @@ http.post('http://192.168.0.6:3000/setting/search', { "user_token" : user_token,
 	res.setEncoding('utf8');
 	res.on('data', function(res) {
 		console.log(res);
-		var resObj = json.parse(res);
+		var resObj = JSON.parse(res);
+		console.log(resObj);
 		water_stop_time = res.ras_watering_operatingtime;
 		shooting_time = res.ras_camera_operatingtime;
 		console.log(water_stop_time);
