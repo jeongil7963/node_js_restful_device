debc3f3a228b6df3af77f88c964da6635e3964ce
양정일 <ya63kr@nate.com>
2017-12-06 12:10:56 +0900

    fix

--- index.js
+++ index.js
@@ -241,8 +241,6 @@ parser.on('data', function(data) {
   });
 });
 
-/*
-
 // 설정 버튼, 사용자 카메라 촬영
 // 소켓 연결
 socket2.on('connect', function() {
@@ -271,5 +269,3 @@ socket2.on(field_id, function(data) {
 socket2.on('disconnect', function() {
   console.log('socket2 disconnected');
 });
-
-*/
